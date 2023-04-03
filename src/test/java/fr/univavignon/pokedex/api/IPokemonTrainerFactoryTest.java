1b7fe08b249d767fa1eb86b68e972e269bbd1286
ELISA-MARTIN <elisa.martin@alumni.univ-avignon.fr>
2023-04-03 20:54:55 +0200

    Test d'un premier test unitaire dans la classe IPokemonTrainerFactoryTest.java

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -0,0 +1,74 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class IPokemonTrainerFactoryTest {
+
+    public IPokemonTrainerFactory getMock() {
+        return Mockito.mock(IPokemonTrainerFactory.class);
+    }
+
+    // A voir si on garde + tard (pour l(instant inutile)
+    @Before
+    public void testRepeat(){
+        IPokemonTrainerFactory trainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
+    }
+
+    @Test
+    public void testIsBetween0And150() {
+        int number = 100; // le nombre à tester
+        assertTrue("Le nombre devrait être compris entre 0 et 150", number >= 0 && number <= 150);
+    }
+
+    @Test
+    public void testCreateTrainerWithMockito() {
+        // crée un objet mock pour l'interface IPokemonTrainerFactory en utilisant Mockito
+        IPokemonTrainerFactory trainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
+        // Création d'un objet simulé
+        PokemonTrainer trainerMock = Mockito.mock(PokemonTrainer.class);
+        // Lorsque le test appellera trainerFactory.createTrainer("Ash Ketchum", Team.VALOR), la méthode renverra trainerMock au lieu de créer une instance de PokemonTrainer normale
+        Mockito.when(trainerFactoryMock.createTrainer(Mockito.anyString(), Mockito.any(Team.class)))
+                .thenReturn(trainerMock);
+
+        PokemonTrainer trainer = trainerFactoryMock.createTrainer("Ash Ketchum", Team.VALOR);
+        assertEquals(trainerMock, trainer);
+    }
+
+    @Test
+    public void parseIntThrowsExceptionWhenNotANumber() throws Exception {
+        assertThrows(NumberFormatException.class, () -> {
+            Integer.parseInt("not a number");
+        });
+    }
+
+    @Test
+    public void testCreateTrainerWithNullName() {
+        // crée un objet mock pour l'interface IPokemonTrainerFactory en utilisant Mockito
+        IPokemonTrainerFactory trainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
+        // Création d'un objet simulé
+        PokemonTrainer trainerMock = Mockito.mock(PokemonTrainer.class);
+        // vérifié si l'exception IllegalArgumentException est levée en utilisant la méthode assertThrows de JUnit
+        assertThrows(IllegalArgumentException.class, () -> {
+            trainerFactoryMock.createTrainer(null, Team.MYSTIC);
+        });
+    }
+
+    @Test
+    public void testParseIntThrowsExceptionWhenNotANumber() {
+        assertThrows(NumberFormatException.class, () -> {
+            Integer.parseInt("not a number");
+        });
+    }
+}
+
+
+
+
