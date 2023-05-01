76a5e1ec9ae5d35824f8e00e7d142b57f40e908f
ELISA-MARTIN <elisa.martin@alumni.univ-avignon.fr>
2023-05-01 15:04:33 +0200

    Modif IPokemonMetadataProviderTest v3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -7,20 +7,6 @@ import static org.junit.Assert.*;
 
 public class IPokemonMetadataProviderTest {
 
-    // Vérifier que PokemonMetadata getPokemonMetadata(int index) throws PokedexException; retourne une exception si l'index n'est pas valide
-    @Test
-    public void testGetPokemonMetadataThrowsPokedexExceptionForInvalidIndex() throws PokedexException {
-        //IPokemonMetadataProvider metadataProviderMock = Mockito.mock(PokemonMetadataProvider.class);
-        PokemonMetadataProvider pokemonMetadataProvider = new PokemonMetadataProvider();
-        int invalidIndex = -1;
-        try {
-            pokemonMetadataProvider.getPokemonMetadata(133);
-            fail("La méthode getPokemonMetadata aurait dû lancer une exception PokedexException");
-        } catch (PokedexException e) {
-            assertEquals("Index de Pokémon invalide : " + invalidIndex, e.getMessage());
-        }
-    }
-
     // Vérifie que la fonction retorune bien un PokemonMetadata
     @Test
     public void testGetPokemonMetadata() throws PokedexException {
