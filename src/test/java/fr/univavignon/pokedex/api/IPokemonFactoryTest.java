a1182606fa2fa072e0c1c3b0dc0d4da88f94abfa
ELISA-MARTIN <elisa.martin@alumni.univ-avignon.fr>
2023-05-01 14:26:43 +0200

    Modif IPokemonFactoryTest v3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -15,7 +15,7 @@ public class IPokemonFactoryTest {
 
     @Test
     public void testCreatePokemon() throws PokedexException {
-        IPokemonFactory pokemonFactory = Mockito.mock(IPokemonFactory.class);
+        IPokemonFactory pokemonFactory = Mockito.mock(PokemonFactory.class);
         IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(PokemonMetadataProvider.class);
         Pokedex pokedex = new Pokedex(pokemonMetadataProvider, pokemonFactory);
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56 );
