f1329f7639a3dfa23ef20566340bd50bdb217478
ELISA-MARTIN <elisa.martin@alumni.univ-avignon.fr>
2023-05-01 15:17:27 +0200

    Modif IPokemonTrainerFactoryTest v3

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -12,47 +12,16 @@ import org.mockito.Mockito;
 
 public class IPokemonTrainerFactoryTest {
 
-    /* public IPokemonTrainerFactory getMock() {
-        return Mockito.mock(IPokemonTrainerFactory.class);
-    } */
-
-    // A voir si on garde + tard (pour l(instant inutile)
-    @Before
-    public void testRepeat(){
-        IPokemonTrainerFactory trainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
-    }
-
-    /*@Test
-    public void testIsBetween0And150() {
-        int number = 100; // le nombre à tester
-        assertTrue("Le nombre devrait être compris entre 0 et 150", number >= 0 && number <= 150);
-    }*/
-
-    /*@Test
+    @Test
     public void testCreateTrainerWithMockito() {
-        // crée un objet mock pour l'interface IPokemonTrainerFactory en utilisant Mockito
-        IPokemonTrainerFactory trainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
-        // Création d'un objet simulé
-        PokemonTrainer trainerMock = Mockito.mock(PokemonTrainer.class);
-        // Lorsque le test appellera trainerFactory.createTrainer("Ash Ketchum", Team.VALOR), la méthode renverra trainerMock au lieu de créer une instance de PokemonTrainer normale
-        Mockito.when(trainerFactoryMock.createTrainer(Mockito.anyString(), Mockito.any(Team.class)))
-                .thenReturn(trainerMock);
+        PokemonTrainerFactory trainerFactory = new PokemonTrainerFactory();
+        PokedexFactory pokedexFactory = new PokedexFactory();
+        PokemonTrainer pokemonTrainer = trainerFactory.createTrainer("Picatchu", Team.VALOR, pokedexFactory);
 
-        PokemonTrainer trainer = trainerFactoryMock.createTrainer("Ash Ketchum", Team.VALOR);
-        assertEquals(trainerMock, trainer);
-    }*/
-
-    /*@Test
-    public void testCreateTrainerWithNullName() {
-        // crée un objet mock pour l'interface IPokemonTrainerFactory en utilisant Mockito
-        IPokemonTrainerFactory trainerFactoryMock = Mockito.mock(IPokemonTrainerFactory.class);
-        // Création d'un objet simulé
-        PokemonTrainer trainerMock = Mockito.mock(PokemonTrainer.class);
-        // vérifié si l'exception IllegalArgumentException est levée en utilisant la méthode assertThrows de JUnit
-        assertThrows(IllegalArgumentException.class, () -> {
-            trainerFactoryMock.createTrainer(null, Team.MYSTIC);
-        });
-    }*/
+        assertEquals(pokemonTrainer.getName(), "Picatchu");
+        assertEquals(pokemonTrainer.getTeam(), Team.VALOR);
+        assertNotNull(pokemonTrainer.getPokedex());
+    }
 }
 
 
