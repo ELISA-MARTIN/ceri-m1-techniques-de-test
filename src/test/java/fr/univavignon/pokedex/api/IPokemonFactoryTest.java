589a23a764d8f11efe463fba7722801d7beca29d
ELISA-MARTIN <elisa.martin@alumni.univ-avignon.fr>
2023-05-05 13:24:11 +0200

    Revue v3 test IpokemonFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -10,8 +10,13 @@ public class IPokemonFactoryTest {
     @Test
     public void testCreatePokemon() throws PokedexException {
         IPokemonFactory pokemonFactory = Mockito.mock(PokemonFactory.class);
+        IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(PokemonMetadataProvider.class);
+        Pokedex pokedex = new Pokedex(pokemonMetadataProvider, pokemonFactory);
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56 );
+        pokedex.addPokemon(pokemon);
+        pokemonFactory.setPokedex(pokedex);
         Pokemon actualPokemon = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+
 
         assertEquals(actualPokemon, pokemon);
         assertTrue(actualPokemon instanceof Pokemon); // Vérifie que l'objet retourné est bien un Pokemon
